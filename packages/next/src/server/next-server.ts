3169b84a924ca7ded454cabcf8e5128abc4b2e00
Hendrik Liebau <mail@hendrik-liebau.de>
2024-07-06 23:50:05 +0200

    Add support for fast refresh cache to edge server

--- packages/next/src/server/next-server.ts
+++ packages/next/src/server/next-server.ts
@@ -1923,6 +1923,7 @@ export default class NextNodeServer extends BaseServer<
       incrementalCache:
         (globalThis as any).__incrementalCache ||
         getRequestMeta(params.req, 'incrementalCache'),
+      fastRefreshFetchCache: this.getFastRefreshFetchCache(),
     })
 
     if (result.fetchMetrics) {
